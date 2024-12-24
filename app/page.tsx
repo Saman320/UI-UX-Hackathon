95a5c6515604ef9e761276230a2b58cc3ab85f4e
Saman Siddiqui <samansiddiqui320@gmail.com>
2024-12-24 21:45:17 +0500

    some change

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,27 @@
-import CarouselFluid from "./components/CarouselFluid";
-import Carousel2 from "./components/Carousel2";
-import DesktopCard from "./components/DesktopCard";
-import DesktopHeader from "./components/DesktopHeader";
-import DesktopProduct from "./components/DesktopProduct";
-import FeaturedPosts from "./components/FeaturedPost";
-import FeaturedPost from "./components/FeaturedPost";
-import Footer from "./components/Footer";
-
-
+import React from 'react'
+import Top_Head from './multiy-components/headers/top-header'
+import Header from './multiy-components/headers/header'
+import Hero from './multiy-components/home/hero-section'
+import Editers from './multiy-components/home/editers'
+import Our_Product from './multiy-components/home/our-product'
+import ClassicSection from './multiy-components/home/clasic-product'
+import Neural from './multiy-components/home/neural'
+import CenteredDetails from './multiy-components/home/future-post'
 
 
 export default function Home() {
   return (
     <div>
-      < DesktopHeader />
-      < DesktopCard />
-      < DesktopProduct />
-      < Carousel2/>
-      < CarouselFluid />
-      < FeaturedPost />
-      < Footer />
+      <Top_Head />
+      <Header/>
+      <Hero />
+      <Editers />
+      <Our_Product />
+      <ClassicSection />
+      <Neural />
+      <CenteredDetails />
       
+   
     </div>
-  );
+  )
 }
